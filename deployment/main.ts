3e4d8a396e5a35f72985b6d645178a22805c02f1
Tim Weise <tim.weise@porsche.digital>
2023-03-15 11:39:41 +0100

    🧱 chore(deployment): setup vercel provider

--- deployment/main.ts
+++ deployment/main.ts
@@ -1,14 +1,45 @@
 import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
+import { Project as VercelProject } from "./.gen/providers/vercel/project";
+import { Deployment as VercelDeployment } from "./.gen/providers/vercel/deployment";
+import { VercelProvider } from "./.gen/providers/vercel/provider";
+import * as dotenv from "dotenv";
 
-class MyStack extends TerraformStack {
-  constructor(scope: Construct, id: string) {
-    super(scope, id);
+dotenv.config();
 
-    // define resources here
-  }
+const APP_CONFIG = {
+  vercelApiToken: process.env.VERCEL_API_TOKEN,
+} as const;
+
+interface NextAppStackProps {
+  vercelApiToken: string;
 }
 
+class NextAppStack extends TerraformStack {
+  constructor(scope: Construct, id: string, props: NextAppStackProps) {
+    super(scope, id);
+
+    new VercelProvider(this, "vercel-provider", {
+      apiToken: props.vercelApiToken,
+    });
+
+    const vercelProject = new VercelProject(this, "vercel-project", {
+      name: "vercel-cdktf-example",
+      framework: "nextjs",
+    });
+
+    new VercelDeployment(this, "vercel-deployment", {
+      projectId: vercelProject.id,
+    });
+  }
+}
+if (!APP_CONFIG.vercelApiToken) {
+  throw new Error(
+    "Please provide the Vercel API Token as environment variable 'VERCEL_API_TOKEN'"
+  );
+}
 const app = new App();
-new MyStack(app, "tempBootstrap");
+new NextAppStack(app, "tempBootstrap", {
+  vercelApiToken: APP_CONFIG.vercelApiToken,
+});
 app.synth();
